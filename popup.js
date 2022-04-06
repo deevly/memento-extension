fd56b6de3715de80a7eb178b13da4e747878cac6
Reason <azsxd34@naver.com>
2022-04-06 23:58:41 +0900

    Init

---
+++ popup.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var clicklistener = document.getElementById("btnStartWorker");
+    // onClick's logic below:
+    clicklistener.addEventListener("click", function () {
+        var port = chrome.extension.connect({
+            name: "Sample Communication",
+        });
+        // background 통신 On 메세지
+        port.postMessage("On");
+        alert("Start Worker");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var clicklistener = document.getElementById("btnStopWorker");
+    // onClick's logic below:
+    clicklistener.addEventListener("click", function () {
+        var port = chrome.extension.connect({
+            name: "Sample Communication",
+        });
+        // background 통신 Off 메세지
+        port.postMessage("Off");
+        alert("Stop Worker");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var clicklistener = document.getElementById("btnLinkToPage");
+    // onClick's logic below:
+    clicklistener.addEventListener("click", function () {
+        var port = chrome.extension.connect({
+            name: "Sample Communication",
+        });
+        // background 통신 Off 메세지
+        port.postMessage("GetUser");
+        port.onMessage.addListener(function (msg) {
+            window.open("http://localhost:8080/log?page=0&name=" + msg);
+        });
+    });
+});
